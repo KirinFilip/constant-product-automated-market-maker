21bb129b4fdd512993573620cd34522bf2f94e45
KirinFilip <filip.kirin.zg1@gmail.com>
2023-03-20 20:37:49 +0100

    storage vars & constructor

--- src/ConstantProductAmm.sol
+++ src/ConstantProductAmm.sol
@@ -1,4 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract ConstantProductAmm {}
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract ConstantProductAmm {
+    /// @notice ERC20 token0 of the pair
+    IERC20 public immutable token0;
+    /// @notice ERC20 token1 of the pair
+    IERC20 public immutable token1;
+
+    /// @notice internal balance of token0
+    uint256 public reserves0;
+    /// @notice internal balance of token1
+    uint256 public reserves1;
+
+    /// @notice total shares (for liquidity)
+    uint256 public totalSupply;
+    /// @notice number of shares per user (for liquidity)
+    mapping(address => uint256) public balanceOf;
+
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+}
--- test/ConstantProductAmm.t.sol
+++ test/ConstantProductAmm.t.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/ConstantProductAmm.sol";
+import {ConstantProductAmm} from "../src/ConstantProductAmm.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract ConstantProductAmmTest is Test {}
+contract Constructor is Test {
+    ERC20 public token0;
+    ERC20 public token1;
+    ConstantProductAmm public CPAMM;
+
+    function test_InitializeTokensCorrectly() public {
+        token0 = new ERC20("Token0", "T0");
+        token1 = new ERC20("Token1", "T1");
+        CPAMM = new ConstantProductAmm(address(token0), address(token1));
+        assertEq(address(CPAMM.token0()), address(token0));
+    }
+}
